803975ed8c92ab2b08ec5fecaed28bbe7435ae3a
Pinal Bhatt <Pinal.Bhatt@weightwatchers.com>
2017-02-09 10:53:29 -0500

    angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {MaterialModule} from '@angular/material';
+
+import 'hammerjs';
+
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { EventListComponent } from './event-list/event-list.component';
+
 
 @NgModule({
   declarations: [
@@ -14,6 +19,7 @@ import { EventListComponent } from './event-list/event-list.component';
   ],
   imports: [
     BrowserModule,
+    MaterialModule.forRoot(),
     FormsModule,
     HttpModule,
     AppRoutingModule
