c6c150ca078727a6b9310ba65f48a55d091807c1
Pinal Bhatt <Pinal.Bhatt@weightwatchers.com>
2017-02-09 09:25:53 -0500

    ng g component event-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { EventListComponent } from './event-list/event-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/event-list/event-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'poc-event-list',
+  templateUrl: './event-list.component.html',
+  styleUrls: ['./event-list.component.sass']
+})
+export class EventListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
